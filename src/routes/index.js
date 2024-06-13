f07370063ea30af38583dfd29c1ca5e467168274
dylanR96 <dylan.ravenson@gmail.com>
2024-06-13 14:17:00 +0200

    added route for user and game, created placeholders for testing

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,13 @@ const { Router } = require("express");
 const router = Router();
 const adminRoute = require("./admin.js");
 const userRoute = require("./user.js");
+const addGame = require("../controller/game/addGameController.js");
+
 router.get("/", (req, res, next) => {
   res.send("Testing one two");
 });
+
+router.post("/game", addGame);
 
 router.use("/admin", adminRoute);
 router.use("/user", userRoute);
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,8 +1,30 @@
 const { Router } = require("express");
 const router = Router();
-const testFunction = require("../controller/users/addUserController.js");
+const addUser = require("../controller/users/addUserController.js");
 router.get("/", (req, res, next) => {
   res.send("Testing user one two");
 });
-router.get("/register", testFunction);
+router.post("/register", addUser);
 module.exports = router;
+
+// Vilka funktioner vill vi ha:
+
+/*
+Users:
+  Förslag på nya ord
+  Starta spel
+    Välj språk (Svenska, Engelska, Båda)
+    Välja antal lag
+    Välja svårighetsgrad per lag
+
+    Standard mode // Först till 40 poäng
+    Max rounds // Välj antal rundor
+
+Admin:
+ - Lägga till nya ord
+ - Acceptera förslagen på nya ord
+
+
+ - Logga in som user/admin
+
+ */
